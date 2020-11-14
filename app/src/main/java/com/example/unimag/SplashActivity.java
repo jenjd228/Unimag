0290fbc6fb3ecc264cae8525979af78dc7008de6
Pavel-Team <Mevedor-Team@gmail.com>
2020-11-14 18:12:58 +0300

    backStack fixed

---
+++ app/src/main/java/com/example/unimag/SplashActivity.java
@@ -0,0 +1,20 @@
+/**СТАРТОВОЕ ОКНО ЗАГРУЗКИ*/
+package com.example.unimag;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
--- app/src/main/java/com/example/unimag/ui/NoConnectionFragment.java
+++ app/src/main/java/com/example/unimag/ui/NoConnectionFragment.java
@@ -1,10 +1,14 @@
 package com.example.unimag.ui;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,8 +19,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.unimag.R;
-import com.example.unimag.ui.catalog.CatalogFragment;
-import com.example.unimag.ui.register.RegisterFragment;
 
 public class NoConnectionFragment extends Fragment {
 
@@ -34,20 +36,47 @@ public class NoConnectionFragment extends Fragment {
         return root;
     }
 
+
+    //Проверка подключения к интернету
+    public static boolean isConnect(Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        button_update_connection = getView().findViewById(R.id.button_update_connection);
+        getActivity().findViewById(R.id.nav_view).setVisibility(View.INVISIBLE); //Убираем меню
 
+        button_update_connection = getView().findViewById(R.id.button_update_connection);
         button_update_connection.setOnClickListener(new View.OnClickListener() { //Установка onClickListener'а для кнопки "Переподключиться"
             @Override
             public void onClick(View v) {
-                FragmentManager manager = getFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(NoConnectionFragment.this.getId(), new CatalogFragment());
-                transaction.addToBackStack(null);
-                transaction.commit();
+                /*manager.popBackStack("last_no_connection_fragment", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                //transaction.addToBackStack(null);
+                transaction.commit();*/
+
+                //new ThreadCheckingConnection(getFragmentManager(), savedInstanceState).execute(); //Если дисконект
+
+                //Если интернет вернулся
+                if (isConnect(requireContext())) {
+                    getActivity().findViewById(R.id.nav_view).setVisibility(View.VISIBLE); //Возвращаем меню
+                    FragmentManager manager = getFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    Toast.makeText(requireContext(), "Подключение восстановлено!",Toast.LENGTH_LONG).show();
+                    manager.popBackStack("last_no_connection_fragment", FragmentManager.POP_BACK_STACK_INCLUSIVE); //Достаем последний фрагмент из стека
+                    transaction.commit();
+                } else {
+                    Toast.makeText(requireContext(), "Нет соединения с интернетом!",Toast.LENGTH_LONG).show();
+                }
             }
         });
 
